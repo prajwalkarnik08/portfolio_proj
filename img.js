d615d28bec37b6848bb1c050cb698f5d18395fda
prajwalkarni08k <prajwalkarnik@gmail.com>
2023-06-07 09:20:13 +0530

    saved

--- img.js
+++
@@ -1,9 +0,0 @@
-let img = document.createElement("img");
-img.src = "https://picsum.photos/200/301";
-
-img.addEventListener("click", function() {
-  console.log("clicked");
-})
-
-document.body.appendChild(img);
-module.exports = {img}
